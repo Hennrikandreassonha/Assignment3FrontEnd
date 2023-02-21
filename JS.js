27f0f4f0513f9848fc4d98e5815180a885d555bc
Henrik1995a@live.se <henrik1995a@live.se>
2023-02-21 13:52:45 +0100

    ja

--- JS.js
+++ JS.js
@@ -1,2 +1,22 @@
+let form = document.querySelector("form");
 
+form.onsubmit = (event) => {
+  event.preventDefault();
 
+  let noteList = document.querySelector("ul");
+
+  let newNote = document.createElement("li");
+
+  let noteText = document.createElement("label");
+  noteText.textContent = form.notetext.value;
+
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  var deleteBtn = document.createElement("button");
+  deleteBtn.id = "remove-btn";
+  deleteBtn.textContent = "❌";
+  newNote.append(checkbox, noteText, deleteBtn);
+
+  noteList.append(newNote);
+};
