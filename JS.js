01a6057ba6ab11439135a5870b533107c96ad29a
Henrik1995a@live.se <henrik1995a@live.se>
2023-02-21 15:50:51 +0100

    pushar

--- JS.js
+++ JS.js
@@ -1,22 +1,126 @@
 let form = document.querySelector("form");
 
+let allNotesList = [];
+
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  let noteList = document.querySelector("ul");
+  let input = form.notetext.value;
 
-  let newNote = document.createElement("li");
+  if (input != "") {
+    let noteList = document.querySelector("ul");
 
-  let noteText = document.createElement("label");
-  noteText.textContent = form.notetext.value;
+    let newNote = document.createElement("li");
+    newNote.addEventListener("mouseover", ShowDeleteBtn);
+    newNote.addEventListener("mouseleave", HideDeleteBtn);
 
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+    let noteText = document.createElement("label");
+    noteText.textContent = input;
 
-  var deleteBtn = document.createElement("button");
-  deleteBtn.id = "remove-btn";
-  deleteBtn.textContent = "❌";
-  newNote.append(checkbox, noteText, deleteBtn);
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("click", CompleteNote);
 
-  noteList.append(newNote);
+    var deleteBtn = document.createElement("button");
+    deleteBtn.id = "remove-btn";
+    deleteBtn.textContent = "❌";
+
+    newNote.append(checkbox, noteText, deleteBtn);
+
+    noteList.append(newNote);
+    allNotesList.push(newNote);
+
+    deleteBtn.addEventListener("click", function (event) {
+      RemoveItem(allNotesList.length - 1);
+    });
+
+    form.reset();
+  }
 };
+
+let allNotes = document.querySelector("#all");
+let activeNotes = document.querySelector("#active");
+let completedNotes = document.querySelector("#complete");
+
+allNotes.addEventListener("click", function () {
+  FilterNotes("all");
+});
+activeNotes.addEventListener("click", function () {
+  FilterNotes("active");
+});
+completedNotes.addEventListener("click", function () {
+  FilterNotes("completed");
+});
+
+function FilterNotes(status) {
+  let noteList = document.querySelector("#to-do-list");
+
+  RemoveListItems();
+
+  if (status == "all") {
+    allNotesList.forEach((element) => {
+      noteList.append(element);
+    });
+  } else if (status == "completed") {
+    allNotesList.forEach((element) => {
+
+      let checkBox = element.querySelector('input[type="checkbox"]');
+
+      if (checkBox.checked) {
+        noteList.append(element);
+      }
+    });
+  } else if (status == "active") {
+    allNotesList.forEach((element) => {
+
+      let checkBox = element.querySelector('input[type="checkbox"]');
+
+      if (!checkBox.checked) {
+        noteList.append(element);
+      }
+    });
+  }
+}
+
+function RemoveListItems() {
+  let noteList = document.querySelector("#to-do-list");
+
+  while (noteList.firstElementChild) {
+    noteList.firstElementChild.remove();
+  }
+}
+
+function RemoveItem(index) {
+  let selectedBtn = event.target;
+  selectedBtn.parentNode.remove();
+
+  //Ta bort från listan
+  allNotesList.splice(index, 1);
+}
+
+function CompleteNote(event) {
+  let selectedCheckbox = event.target;
+  let noteLiElement = selectedCheckbox.parentNode;
+
+  let noteText = noteLiElement.querySelector("label");
+
+  if (selectedCheckbox.checked) {
+    noteText.style.textDecoration = "line-through";
+  } else {
+    noteText.style.textDecoration = "none";
+  }
+}
+function ShowDeleteBtn(event) {
+  let liElement = event.target;
+  let deleteBtn = liElement.querySelector("button");
+
+  if (deleteBtn != null) {
+    deleteBtn.style.visibility = "visible";
+  }
+}
+function HideDeleteBtn(event) {
+  let liElement = event.target;
+  let deleteBtn = liElement.querySelector("button");
+
+  deleteBtn.style.visibility = "hidden";
+}
