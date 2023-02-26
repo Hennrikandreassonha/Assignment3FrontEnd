9ca564c7df4979d7118037ca17de5a5582d0194c
Henrik1995a@live.se <henrik1995a@live.se>
2023-02-26 14:00:45 +0100

    Tests ok

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -23,8 +23,16 @@ test('One item left controll', async ({ page }) => {
 test('3 Notes test', async ({ page }) => {
   await page.goto('http://127.0.0.1:5501/Assignment3FrontEnd/index.html');
 
-  await page.fill('input[type="text"]', 'Ny notering');
+  await page.fill('input[type="text"]', 'Note 1');
   await page.keyboard.press('Enter');
 
-  await expect(page.locator("text = 1 item left")).toBeVisible();
+  await page.fill('input[type="text"]', 'Note 2');
+  await page.keyboard.press('Enter');
+
+  await page.fill('input[type="text"]', 'Note 3');
+  await page.keyboard.press('Enter');
+
+  await page.getByRole('checkbox').first().check();
+
+  await expect(page.locator("text = 2 items left")).toBeVisible();
 });
